6b0a27e79d883a2e01aae2f6827f8146f01593d2
nejcik <nataliaxskowronek@gmail.com>
2017-08-10 12:07:35 +0200

    migrated db review_post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810091223) do
+ActiveRecord::Schema.define(version: 20170810095140) do
+
+  create_table "review_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
