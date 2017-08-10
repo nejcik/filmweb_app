826687d7dc0de7e0f795ad5c03825e3d776e1cd8
nejcik <nataliaxskowronek@gmail.com>
2017-08-11 01:20:14 +0200

    Fine working static pages with users and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
 
   resources :users
+  resources :review_posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-
+  root 'static_pages#home'
+  get 'review_posts/show'
 end
