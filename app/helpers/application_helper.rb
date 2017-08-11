160ca66552125103f8f3e3a306724879bb0c8a64
nejcik <nataliaxskowronek@gmail.com>
2017-08-11 19:55:28 +0200

    Change test file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Returns the full title on per-page basis.
+  def full_title(page_title='')
+    base_title= "FilmwebApp"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
