f5cab797b0003885148d94f62bd2d1d558b2068f
nejcik <nataliaxskowronek@gmail.com>
2017-08-16 16:27:47 +0200

    has_secure password function + installed bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-  before_save { email.downcase!} #this does not work! bcause of :!!!!
+  before_save { email.downcase!} #this does not work! bcause of :!!!! now ok ;)
   has_many :review_posts
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-._]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
+
+  #single Rails method -> secure password machinery
+  has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816115533) do
+ActiveRecord::Schema.define(version: 20170816131712) do
 
   create_table "review_posts", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170816115533) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
